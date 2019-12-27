391f7f40302275ab459ba26b737ee424555de982
Harpreet Singh <snghar002@myuct.ac.za>
2019-12-27 15:59:39 +0200

    Updates

---
+++ precache-manifest.2b5d99e49918a1651eb136df5b4429df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f868ee68e3b2c4d86ac7df7c886db3ce",
+    "url": "/zaiostuff/index.html"
+  },
+  {
+    "revision": "3bda0f52bd72c79574b1",
+    "url": "/zaiostuff/static/js/2.116af629.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/zaiostuff/static/js/2.116af629.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9b67dd1565a0450bf6b",
+    "url": "/zaiostuff/static/js/main.6785a2a1.chunk.js"
+  },
+  {
+    "revision": "11ab9cd6a00bc67b7a5f",
+    "url": "/zaiostuff/static/js/runtime-main.7b05f0c0.js"
+  }
+]);
